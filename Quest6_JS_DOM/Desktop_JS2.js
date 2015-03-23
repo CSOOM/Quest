696158992caaceb4560ef7c9bf16899d7b42b2f1
CSOOM <cathycho1221@gmail.com>
2015-03-23 19:23:19 +0900

    Quest6 Desktop Modify Finish

--- Quest6_JS_DOM/Desktop_JS2.js
+++ Quest6_JS_DOM/Desktop_JS2.js
@@ -33,8 +33,11 @@ _._setDom = function() {
 	this.dom.className = 'icon ' + this.type;
 	this.dom.style.left = (this.desktop.icons.length % 9) * 110 + 'px';
 	this.dom.style.top = Math.floor(this.desktop.icons.length / 9) * 110 + 'px';
-
 	this.desktop.dom.appendChild(this.dom);
+
+	var x = this.dom.style.left;
+	var y = this.dom.style.top;
+	this.mCoordNow = [x,y];
 };
 
 _._bindEvents = function() {
@@ -43,13 +46,12 @@ _._bindEvents = function() {
 	this.dom.ondblclick = function() {
 		if(that.type !== 'folder') return;
 
-		that.desktop.windows.push(new Win(that.desktop, that));
+		that.desktop.windows.push(new Win(that.desktop, that, that.mCoordNow));
 	};
 
 	this.dom.onmousedown = function(e) {
 		that.isMouseDown = true;
 		that.mCoordNow = [e.clientX, e.clientY];
-		that.dom.className += ' onmousedown';
 	};
 
 	this.dom.onmousemove = function(e) {
@@ -65,132 +67,69 @@ _._bindEvents = function() {
 
 	this.dom.onmouseup = function() {
 		that.isMouseDown = false;
-		that.dom.className = that.dom.className.replace(' onmousedown', '');
 	};
 
 	this.dom.onmouseleave = function() {
 		that.isMouseDown = false;
-		that.dom.className = that.dom.className.replace(' onmousedown', '');
 	};
 };
 
 
-var Win = function(desktop, icon) {
-		this.desktop = desktop;
-		this.icon = icon;
-		this.type = icon.type;
-
-		this._initialize();
-	};
+var Win = function(desktop, icon, mCoordNow) {
+	this.desktop = desktop;
+	this.icon = icon;
+	this.type = icon.type;
+	this.mCoordNow = mCoordNow;
+	this.isMouseDown = false;
+	this._initialize();
+};
 
 var _ = Win.prototype;
 
 _._initialize = function() {
-	// this._setDom();
-	// this._bindEvents();
+	this._setDom();
+	this._bindEvents();
 };
 
-// function Icon(name){
-// 	this.name = name;
-// 	//this.drag = function(){};
-// }
-// function Folder(name, Window){
-// 	this.name = name;
-// 	this.window = Window;
-// 	//this.open = function(){}; //더블클릭하면 폴더가 열리는 함수
-// 	//this.click = function(){};
-// }
-// function Window(content){//열린 폴더의 창
-// 	this.content = content;
-// 	//this.drag = function(){}
-// }
+_._setDom = function(){
+	this.dom = document.createElement('div');
+	this.dom.className = 'win';
+	this.desktop.dom.appendChild(this.dom);
+	this.dom.style.left = this.mCoordNow[0]+'px'; 
+	this.dom.style.top = this.mCoordNow[1]+'px'; 
+};
 
-// Folder.prototype = new Icon(); //inherit
+_._bindEvents = function(){
+	var that = this;
 
+	this.dom.ondblclick = function(){
+		console.log(this);
+		that.desktop.dom.removeChild(this);
+	};
 
-// var window1 = new Window("Hello world");
-// var window2 = new Window("Bye world");
+	this.dom.onmousedown = function(e) {
+		that.isMouseDown = true;
+		that.mCoordNow = [e.clientX, e.clientY];
+	};
 
-// var icon1 = new Icon("chrome");
-// var icon2 = new Folder("bird", window1);
-// var icon3 = new Folder("pigeon", window2);
+	this.dom.onmousemove = function(e) {
+		if(!that.isMouseDown) return;
 
-// //--------------------------------------------------------------------------이전 퀘스트
+		var delta = [e.clientX - that.mCoordNow[0], e.clientY - that.mCoordNow[1]];
 
-// document.getElementById('chrome').onmousedown = function() {
-// 	var icon = this;
+		that.dom.style.left = Number(that.dom.style.left.replace('px', '')) + delta[0] + 'px';
+		that.dom.style.top = Number(that.dom.style.top.replace('px', '')) + delta[1] + 'px';
 
-// 	document.onmousemove = function(e) {
-// 		e = e || event
-// 		icon.style.left = e.clientX-60+'px'
-// 		icon.style.top = e.clientY-60+'px'
-// 	}
+		that.mCoordNow = [e.clientX, e.clientY];
+	};
 
-// 	this.onmouseup = function() {
-// 		document.onmousemove = null
-// 	}
+	this.dom.onmouseup = function() {
+		that.isMouseDown = false;
+	};
 
-// }
-// document.getElementById('chrome').ondragstart = function() { return false }
+	this.dom.onmouseleave = function() {
+		that.isMouseDown = false;
+	};
+};
 
 
-// document.getElementById('folder1').onmousedown = function() {
-// 	var icon = this;
-
-// 	document.onmousemove = function(e) {
-// 		e = e || event
-// 		icon.style.left = e.clientX-60+'px'
-// 		icon.style.top = e.clientY-60+'px'
-// 	}
-
-// 	this.onmouseup = function() {
-// 		document.onmousemove = null
-// 	}
-
-// }
-// document.getElementById('folder1').ondragstart = function() { return false }
-
-
-// document.getElementById('folder2').onmousedown = function() {
-// 	var icon = this
-
-// 	document.onmousemove = function(e) {
-// 		e = e || event
-// 		icon.style.left = e.clientX-60+'px'
-// 		icon.style.top = e.clientY-60+'px'
-// 	}
-
-// 	this.onmouseup = function() {
-// 		document.onmousemove = null
-// 	}
-
-// }
-// document.getElementById('folder2').ondragstart = function() { return false }
-
-// document.getElementById('folder1').ondblclick = function(e) {
-// 	var x = e.clientX;
-// 	var y = e.clientY;
-
-// 	popwin1 = window.open("","popwin1", "width=400, height=300");
-// 	popwin1.document.write("I am folder1");
-// 	popwin1.moveTo(x,y);
-// 	popwin1.focus();
-// }
-
-// document.getElementById('folder2').ondblclick = function(e) {
-// 	var x = e.clientX;
-// 	var y = e.clientY;
-
-// 	popwin2 = window.open("","popwin2", "width=400, height=300");
-// 	popwin2.document.write("I am folder2");
-// 	popwin2.moveTo(x,y);
-// 	popwin2.focus();
-// }
-
-// //document.getElementById('rest').ondragover= function(){return false;}
-// document.getElementById('rest').ondragstart= function(){return false;}
-// document.getElementById('rest').ondrag= function(){return false;}
-// document.getElementById('rest').ondragend= function(){return false;}
-// document.getElementById('rest').ondragenter= function(){return false;}
-
-// //배경화면 이외는 드래그 못하게 하기 
