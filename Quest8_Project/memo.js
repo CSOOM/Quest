685529f3150c49edc3519f12ccc899edb36399f2
CSOOM <cathycho1221@gmail.com>
2015-03-30 18:13:48 +0900

    추가기능 아직 완성 못함

--- Quest8_Project/memo.js
+++ Quest8_Project/memo.js
@@ -1,7 +1,6 @@
 var Background = function(dom) {
 	this.dom = dom;
 	this.categories = [];
-	this.memo = [];
 };
 
 var _ = Background.prototype;
@@ -11,30 +10,32 @@ _.addCategory = function(category){
 };
 
 var Button_addCategory = function(back, dom){
-	this.dom = dom;
 	this.back = back;
-
+	this.dom = dom;
+	
 	this._initialize();
 };
 
 var _ = Button_addCategory.prototype;
 
-_.initialize = function(){
+_._initialize = function(){
 	this._bindEvents();
 }
 
 _._bindEvents = function(){
 	var that = this;
-
 	this.dom.onclick = function(){
-
+		var name = document.getElementsByClassName("addCate name")[0].value;
+		var color = document.getElementsByClassName("addCate color")[0].value;
+		that.back.addCategory(new Category(that.back, name, color));
 	}
-}//미완성 카테고리 더하는 거 구현하기!!!!!!!
+};
 
 var Category = function(back, name, color){
 	this.back = back;
 	this.name = name;
 	this.color = color;
+	this.memo = [];
 
 	this._initialize();
 };
@@ -43,16 +44,96 @@ var _ = Category.prototype;
 
 _._initialize = function(){
 	this._setDom();
-//	this._bindEvents();
+	this._bindEvents();
 };
+
 
 _._setDom = function(){
 	this.dom = document.createElement('div');
 	this.dom.className = 'category';
-	this.dom.style.left = (this.back.categories.length % 3) * 630 + 20 + 'px';
-	this.dom.style.top = Math.floor(this.back.categories.length / 3) * 420 + 10 + 'px';
+	this.dom.style.left = (this.back.categories.length % 3) * 635 + 15 + 'px';
+	this.dom.style.top = Math.floor(this.back.categories.length / 3) * 425 + 15 + 'px';
 	this.dom.style.backgroundColor = this.color;
 	this.dom.innerHTML = this.name;
 	this.back.dom.appendChild(this.dom);
+
+	var add_Memo = new Button_addMemo(this);
+};
+
+_._bindEvents = function(){
+	var that = this;
+}
+
+_.addMemo = function(memo){
+	this.memo.push(memo);
+};
+
+var Button_addMemo = function(category){
+	this.category = category;
+
+	this._initialize();
+};
+
+var _ = Button_addMemo.prototype;
+
+_._initialize = function(){
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function(){
+	this.dom = document.createElement('div');
+	this.dom.className = 'addMemo';
+	this.category.dom.appendChild(this.dom);
+}
+
+_._bindEvents = function(){
+	var that = this;
+	this.dom.onclick = function(){
+		that.category.addMemo(new Memo(that.category, "Title", "Content"));
+	}
+}
+
+
+
+var Memo = function(category, title, content){
+	this.category = category;
+	this.title = title;
+	this.content = content;
+
+	this._initialize();
+};
+
+var _ = Memo.prototype;
+
+_._initialize = function(){
+	this._setDom();
+	this._bindEvents();
+};
+
+_._setDom = function(){
+	this.dom = document.createElement('div');
+	this.dom.className = 'memo';
+	this.dom.style.left = (this.category.memo.length % 2) * 290 + 20 + 'px';
+	this.dom.style.top = Math.floor(this.category.memo.length / 2) * 360 + 45 + 'px';
+	this.category.dom.appendChild(this.dom);
+
+	var title = document.createElement('input');
+	title.type = "text";
+	title.className = "input_title";
+	title.value = this.title;
+	this.dom.appendChild(title);
+
+	var content = document.createElement('textarea');
+	content.className = "textarea_content";
+	content.value = this.content;
+	this.dom.appendChild(content);
+
+};
+
+_._bindEvents = function(){
+	var that = this;
+
+
 };
 
