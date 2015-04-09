585821c0a5a899875f231f2b8c875443fc7d5c08
CSOOM <cathycho1221@gmail.com>
2015-04-09 15:54:46 +0900

    Quest11 Finish

---
+++ Quest11_HTTP server/server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (request, response){
+	var query = url.parse(request.url, false).query;
+
+
+	fs.readFile('page.html', function(error, data){
+		var html;
+		if(request.method == 'GET'){
+			console.log(query);
+			html = data+query;
+			response.writeHead(200);
+			response.write(html);
+			response.end();
+		}else if(request.method == 'POST'){
+			request.on('data', function(text){
+				console.log(text);
+				html = data+text;
+				response.writeHead(200);
+				response.write(html);
+				response.end();
+			});
+		}
+
+	});
+
+
+}).listen(2222, function(){
+	console.log("Server Running at http://localhost:2222");
+});
